683b1eacb2b561f2423385ebd9b7d6165ecca017
xiaoguang yang <epoluodi@xiaoguangmatoMacBook-Pro.local>
2015-02-27 23:34:43 +0800

    Initial Commit

---
+++ 泽臻小店/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  泽臻小店
+//
+//  Created by xiaoguang yang on 15-2-27.
+//  Copyright (c) 2015年 Apollo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 泽臻小店/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  泽臻小店
+//
+//  Created by xiaoguang yang on 15-2-27.
+//  Copyright (c) 2015年 Apollo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
